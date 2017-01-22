1606ee6347f355d09f3726dd479d0a21a39884c0
hao <857968442@qq.com>
2017-01-22 17:40:53 +0800

    加上了webapp和web.xml

--- src/aurora/timer/server/service/impl/UserOnlineTimeServiceImpl.java
+++ src/aurora/timer/server/service/impl/UserOnlineTimeServiceImpl.java
@@ -22,8 +22,7 @@ import java.util.logging.Logger;
 public class UserOnlineTimeServiceImpl implements IUserOnlineTimeService {
     Logger logger = Logger.getLogger("UserOnlineTime");
     /**
-     * 增加时间就靠这个了！每5分钟上传一次，如果距离最近在线时间小于10分钟的话则加时<br>
-     * <li>否则更新最近在线时间并加上5分钟</li>
+     * 增加时间就靠这个了！每5分钟上传一次，如果距离最近在线时间小于10分钟的话则加时
      * @param id 在线的用户的ID
      * @return 加时成功返回true，失败返回false
      * @throws Exception
