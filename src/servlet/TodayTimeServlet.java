d38cbfb729b86f4ad09c15dce7fb0cb919f8f3f7
hao <857968442@qq.com>
2017-01-30 23:11:48 +0800

    完成今日和本周

--- src/servlet/TodayTimeServlet.java
+++ src/servlet/TodayTimeServlet.java
@@ -4,54 +4,38 @@ import aurora.timer.server.factory.ServiceFactory;
 import aurora.timer.server.service.iservice.IUserOnlineTimeService;
 import aurora.timer.server.vo.UserOnlineTime;
 import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by hao on 17-1-30.
  */
-public class ThisWeekServlet extends HttpServlet {
+public class TodayTimeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         resp.setContentType("application/json;charset=UTF-8");
         resp.setCharacterEncoding("UTF-8");
 
         IUserOnlineTimeService iuots = ServiceFactory.getIUserOnlineTimeService();
         Set<UserOnlineTime> set;
-        Map<String, Long> map = new HashMap<>();
         JSONObject object = new JSONObject();
         JSONObject temp;
         try {
-            set = iuots.thisWeekData();
-            Iterator<UserOnlineTime> setIt = set.iterator();
+            set = iuots.todayData();
+            Iterator<UserOnlineTime> iterator = set.iterator();
             UserOnlineTime vo;
-            while (setIt.hasNext()) {
-                vo = setIt.next();
-                Long t;
-                if ((t = map.get(vo.getID())) != null) {
-                    map.put(vo.getID(), t + vo.getTodayOnlineTime());
-                }
-            }
-
-            Iterator<String> mapIt = map.keySet().iterator();
-            while (mapIt.hasNext()) {
-                String s = mapIt.next();
+            while (iterator.hasNext()) {
+                vo = iterator.next();
                 temp = new JSONObject();
-                temp.put("id", s);
-                temp.put("time", map.get(s));
-                object.put(s, temp);
+                temp.put("id", vo.getID());
+                temp.put("time", vo.getTodayOnlineTime().toString());
+                object.put(vo.getID(), temp);
             }
 
             resp.getWriter().println(object.toJSONString());
