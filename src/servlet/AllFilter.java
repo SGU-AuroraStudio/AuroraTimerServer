93e5d36c6eed651c6bb9492eccc20331299058e2
hao <lancelot9_geek@qq.com>
2017-03-20 20:45:57 +0800

    没事上传一下

--- src/servlet/AllFilter.java
+++ src/servlet/AllFilter.java
@@ -19,19 +19,17 @@ public class AllFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        //超过晚上10点以及小于早上7点不予计时
-//        LocalTime time = LocalTime.now();
-//        boolean flag = false;
-//        for (int i = 0; i < ClientAddr.addr.length; i ++) {
-//            if (ClientAddr.addr[i].equals(servletRequest.getRemoteAddr())) {
-//                flag = true;
-//            }
-//        }
-//        if (!(time.getHour() > 22 || time.getHour() < 7) && flag) {
-//            filterChain.doFilter(servletRequest, servletResponse);
-//        }
-//        throw new ServletException();
-        filterChain.doFilter(servletRequest,servletResponse);
+        //超过晚上11点以及小于早上6点不予计时
+        LocalTime time = LocalTime.now();
+        boolean flag = false;
+        for (int i = 0; i < ClientAddr.addr.length; i ++) {
+            if (ClientAddr.addr[i].equals(servletRequest.getRemoteAddr())) {
+                flag = true;
+            }
+        }
+        if (!(time.getHour() > 22 || time.getHour() < 7) && flag) {
+            filterChain.doFilter(servletRequest, servletResponse); //放行
+        }
     }
 
     @Override
