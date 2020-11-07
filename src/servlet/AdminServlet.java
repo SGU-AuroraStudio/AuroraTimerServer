71879cb5dcdb04d2ac003bc34c2a67e2cef6e95d
18125061059-YaoRenzhao <40916709+18125061059-YaoRenzhao@users.noreply.github.com>
2020-11-07 13:57:06 +0800

    增加isFreeTime()返回是否在放挂时间
    需要get带上参数 x=freeTime

--- src/servlet/AdminServlet.java
+++ src/servlet/AdminServlet.java
@@ -23,17 +23,60 @@ public class AdminServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getParameter("x") == null)
+            returnAdminData(req, resp);
+        else if (req.getParameter("x").equals("freeTime")) {
+            resp.getWriter().println(isFreeTime(req, resp)); //返回是否在放挂时间内
+            resp.getWriter().close();
+        } else
+            returnAdminData(req, resp);
+    }
+
+    /**
+     * 返回管理员数据,返回的数据里时间是long类型。
+     * 即SQL里Time类型的数据在传递的时候用Long
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("updateAdminData");
+        //TODO:检查id是否为管理员
+        resp.setContentType("text/txt;charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        req.setCharacterEncoding("UTF-8");
+
+        boolean flag = false;
+        IAdminDataService iads = ServiceFactory.getIAdminDataService();
+        try {
+            AdminData data = new AdminData();
+            data.setAnnouncement(req.getParameter("announcement"));
+            data.setDutylist(req.getParameter("dutyList"));
+            System.out.println(data.getDutylist());
+            data.setFreeTimeStart(new Time(Long.parseLong(req.getParameter("freeTimeStart"))));
+            data.setFreeTimeEnd(new Time(Long.parseLong(req.getParameter("freeTimeEnd"))));
+
+            flag = iads.updateAdminData(data);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (flag)
+            resp.getWriter().println("true");
+        else
+            resp.getWriter().println("false");
+    }
+
+    public void returnAdminData(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("returnAdminData");
         resp.setContentType("text/txt;charset=UTF-8");
         resp.setCharacterEncoding("UTF-8");
         IAdminDataService iads = ServiceFactory.getIAdminDataService();
         try {
             AdminData data = iads.getAdminData();
             JSONObject object = new JSONObject();
-            if(data!=null){
-                object.put("announcement",data.getAnnouncement());
-                object.put("dutyList",data.getDutylist());
-                object.put("freeTimeStart",data.getFreeTimeStart().getTime());
-                object.put("freeTimeEnd",data.getFreeTimeEnd().getTime());
+            if (data != null) {
+                object.put("announcement", data.getAnnouncement());
+                object.put("dutyList", data.getDutylist());
+                object.put("freeTimeStart", data.getFreeTimeStart().getTime());
+                object.put("freeTimeEnd", data.getFreeTimeEnd().getTime());
             }
             resp.getWriter().println(object.toJSONString());
             resp.getWriter().close();
@@ -42,28 +85,15 @@ public class AdminServlet extends HttpServlet {
         }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO:检查id是否为管理员
-        resp.setContentType("text/txt;charset=UTF-8");
-        resp.setCharacterEncoding("UTF-8");
-
-        boolean flag=false;
+    public boolean isFreeTime(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         IAdminDataService iads = ServiceFactory.getIAdminDataService();
         try {
-            AdminData data = new AdminData();
-            data.setAnnouncement(req.getParameter("announcement"));
-            data.setDutylist(req.getParameter("dutyList"));
-            data.setFreeTimeStart(new Time(Long.parseLong(req.getParameter("freeTimeStart"))));
-            data.setFreeTimeEnd(new Time(Long.parseLong(req.getParameter("freeTimeEnd"))));
-
-            flag = iads.updateAdminData(data);
+            AdminData adminData = iads.getAdminData();
+            long systemCurrentTimeLong = System.currentTimeMillis();
+            return systemCurrentTimeLong > adminData.getFreeTimeStart().getTime() && systemCurrentTimeLong < adminData.getFreeTimeEnd().getTime() + 1200000; //向后移20分钟，防止在例如17：01分的时候返回true
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if(flag)
-            resp.getWriter().println("true");
-        else
-            resp.getWriter().println("false");
+        return true;
     }
 }
