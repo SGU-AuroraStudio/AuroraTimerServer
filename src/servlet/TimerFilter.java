8a4e08ec48ae8986b981c9c0a5b76eb3913b7bc6
18125061059-YaoRenzhao <40916709+18125061059-YaoRenzhao@users.noreply.github.com>
2020-12-01 15:37:31 +0800

    Update TimerFilter.java

--- src/servlet/TimerFilter.java
+++ src/servlet/TimerFilter.java
@@ -21,7 +21,7 @@ public class TimerFilter implements Filter {
         //超过晚上11点以及小于早上6点不予计时
         LocalTime time = LocalTime.now();
         boolean flag = false;
-        for (int i = 0; i < ClientAddr.addr.length; i ++) {
+        for (int i = 0; i < ClientAddr.addr.length; i++) {
             if (ClientAddr.addr[i].equals(servletRequest.getRemoteAddr()) || servletRequest.getRemoteAddr().contains(ClientAddr.addr[i])) {
                 flag = true;
             }
@@ -29,8 +29,11 @@ public class TimerFilter implements Filter {
         if (!(time.getHour() > 22 || time.getHour() < 7) && flag) {
             filterChain.doFilter(servletRequest, servletResponse); //放行
         }
-        if(!flag)
-            servletResponse.getWriter().println("请在工作室计时。（如果您已经在工作室，请联系管理员。\n出bug了，先检查工作室ip有没有变） ");
+        if (!flag) {
+            ServletContext servletContext = servletRequest.getServletContext();
+            servletContext.log(servletRequest.getRemoteAddr() + "请求计时失败");
+            servletResponse.getWriter().println("请在工作室计时。（如果您已经在工作室，请联系管理员。\n出bug了，先检查工作室ip有没有变）");
+        }
     }
 
     @Override
